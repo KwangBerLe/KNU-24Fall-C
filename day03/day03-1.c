c706856b8314b45dc16e3843e40eda2742efb26e
KwangBerLe <gamj4525@gmail.com>
2024-09-19 13:48:57 +0900

    소수판별 함수 실습 01
    1과 자기 자신으로만 나눠지는 수

---
+++ day03/day03-1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int PrimeNum(int num);
+
+int main() {
+	int num;
+	printf("소수인지 확인할 수를 입력하세요 : ");
+	scanf_s("%d", &num);
+
+	if (PrimeNum(num))
+		printf("해당 수는 소수입니다.");
+	else
+		printf("해당 수는 소수가 아닙니다.");
+
+	return 0;
+}
+
+int PrimeNum(int num) {
+	int sum = 0;
+	for (int i = 1; i <= num; i++) {
+		if (num % i == 0)
+			sum++;
+	}
+	if (sum == 2)
+		return 1;
+	else
+		return 0;
+}
