2b034c354f2257168d32022d8ac1de7e7729184b
KwangBerLe <gamj4525@gmail.com>
2024-09-19 14:26:27 +0900

    함수사용 실습
    재귀함수 팩토리얼 실습

---
+++ day03/day03-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fac(int num);
+
+int main() {
+	int num = 0;;
+	printf("계산할 팩토리얼 값 입력 : ");
+	scanf_s("%d", &num);
+
+	printf("%d! = %d", num, fac(num));
+
+	return 0;
+}
+
+int fac(int num) {
+	int result = 0;
+	if (num == 1)
+		return 1;
+
+	result = num * fac(num - 1);
+
+	return result;
+}
