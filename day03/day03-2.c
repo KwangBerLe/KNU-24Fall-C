e6d5dccf897a5071d9a317404df536bbc67ae56b
KwangBerLe <gamj4525@gmail.com>
2024-09-19 14:43:19 +0900

    재귀함수 실습
    예외처리 추가

--- day03/day03-2.c
+++ day03/day03-2.c
@@ -3,7 +3,7 @@
 int fac(int num);
 
 int main() {
-	int num = 0;;
+	int num = 0;
 	printf("계산할 팩토리얼 값 입력 : ");
 	scanf_s("%d", &num);
 
@@ -14,8 +14,8 @@ int main() {
 
 int fac(int num) {
 	int result = 0;
-	if (num == 1)
-		return 1;
+	if (num == 0 || num == 1)
+		return result;
 
 	result = num * fac(num - 1);
 
